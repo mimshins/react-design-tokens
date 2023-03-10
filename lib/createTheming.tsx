ba1d44addbcfc0a744c4d42cd2af243f2fea4ed0
mimshins <mostafa.sh.coderino@gmail.com>
2023-03-10 04:16:29 +0330

    [✨] add `createTheming` core

---
+++ lib/createTheming.tsx
@@ -0,0 +1,128 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import * as React from "react";
+import { deepMerge, isPlainObject, shallowMerge } from "./helpers";
+
+type AnyObject = Record<keyof any, any>;
+
+type DeepPartial<T> = {
+  [P in keyof T]?: T[P] extends AnyObject ? DeepPartial<T[P]> : T[P];
+};
+
+interface ThemeProviderProps<T> {
+  children: React.ReactNode;
+  theme: DeepPartial<T>;
+}
+
+interface CSSVariableProviderProps<T> {
+  children: React.ReactNode;
+  theme: DeepPartial<T>;
+}
+
+interface ThemingConfig<T> {
+  mergeStrategy?: "shallow" | "deep";
+  cssVariableGenerator?: (
+    tokenFamilyKey: keyof T,
+    tokenPath: string,
+    tokenValue: unknown
+  ) => { variable: string; value: string } | null;
+}
+
+const defaultCssVariableGenerator = (
+  tokenFamilyKey: string,
+  tokenPath: string,
+  tokenValue: unknown
+) => {
+  if (!["string", "number"].includes(typeof tokenValue)) return null;
+
+  const segments = tokenPath
+    .split(".")
+    .filter(Boolean)
+    .map(s => s.toLowerCase());
+
+  return {
+    variable: `${tokenFamilyKey.toLowerCase()}${
+      segments.length > 0 ? "-".concat(segments.join("-")) : ""
+    }`,
+    value:
+      typeof tokenValue === "number" ? `${tokenValue}px` : String(tokenValue)
+  };
+};
+
+const createTheming = <T extends AnyObject>(config?: ThemingConfig<T>) => {
+  const {
+    mergeStrategy = "deep",
+    cssVariableGenerator = defaultCssVariableGenerator
+  } = config ?? {};
+
+  const merge = mergeStrategy === "deep" ? deepMerge : shallowMerge;
+
+  const ThemeContext = React.createContext<T | null>(null);
+
+  const useTheme = (): T | null => React.useContext(ThemeContext);
+
+  const generateCssVariables = (
+    theme: AnyObject,
+    path: string[] = []
+  ): Array<{ variable: string; value: string } | null> => {
+    return Object.entries(theme)
+      .map(([key, value]) => {
+        const newPath = [...path, key];
+
+        if (!isPlainObject(value)) {
+          return cssVariableGenerator(
+            newPath[0] ?? key,
+            newPath.slice(1).join("."),
+            value
+          );
+        }
+
+        return generateCssVariables(value as AnyObject, newPath);
+      })
+      .flat();
+  };
+
+  const CSSVariableProvider = (props: CSSVariableProviderProps<T>) => {
+    const { children, theme } = props;
+
+    const generatedVariables = React.useMemo(
+      () => generateCssVariables(theme),
+      [theme]
+    );
+
+    const refCallback = (node: HTMLDivElement | null) => {
+      if (!node) return;
+      generatedVariables.forEach(v => {
+        if (!v) return;
+
+        node.style.setProperty(`--${v.variable}`, v.value);
+      });
+    };
+
+    return (
+      <div ref={refCallback} data-name="CSSVariableProvider">
+        {children}
+      </div>
+    );
+  };
+
+  const ThemeProvider = (props: ThemeProviderProps<T>): JSX.Element => {
+    const { children, theme: localTheme } = props;
+
+    const outerTheme = useTheme();
+
+    const theme = React.useMemo<T>(
+      () => (outerTheme ? merge(outerTheme, localTheme) : localTheme) as T,
+      [localTheme, outerTheme]
+    );
+
+    return (
+      <ThemeContext.Provider value={theme}>
+        <CSSVariableProvider theme={localTheme}>{children}</CSSVariableProvider>
+      </ThemeContext.Provider>
+    );
+  };
+
+  return { useTheme, ThemeProvider };
+};
+
+export default createTheming;
