abb8b7aba96e465e47835401b87d9f097d599ea3
mimshins <mostafa.sh.coderino@gmail.com>
2024-04-07 16:24:52 +0330

    Remove redundant files

--- lib/helpers/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as deepMerge } from "./deepMerge";
-export { default as isPlainObject } from "./isPlainObject";
-export { default as shallowMerge } from "./shallowMerge";
-export { default as useIsomorphicLayoutEffect } from "./useIsomorphicLayoutEffect";
--- lib/helpers/useIsomorphicLayoutEffect.ts
+++
@@ -1,6 +0,0 @@
-import * as React from "react";
-
-const useIsomorphicLayoutEffect =
-  typeof window === "undefined" ? React.useEffect : React.useLayoutEffect;
-
-export default useIsomorphicLayoutEffect;
